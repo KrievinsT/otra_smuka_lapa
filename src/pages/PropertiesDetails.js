48489afd458be671feb7b221fd49373651494eae
JustsPuspurs <144904136+JustsPuspurs@users.noreply.github.com>
2024-12-02 16:43:59 +0200

    Small Change

--- src/pages/PropertiesDetails.js
+++ src/pages/PropertiesDetails.js
@@ -3,7 +3,7 @@ import Button from "../components/Button";
 function PropertiesDetails() {
   return (
     <section className="container mx-auto text-center py-20 px-4">
-      <Button text="Contact us" variant="darkdefault" />
+
     </section>
   );
 }
